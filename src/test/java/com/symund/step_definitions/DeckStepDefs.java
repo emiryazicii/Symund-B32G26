094cc0d3252725ebf4816ca2ccdb32b1998f6de7
European01 <emiryaziciofficial@gmail.com>
2024-03-20 05:21:49 -0400

    Update DeckStepDefs.java class.

---
+++ src/test/java/com/symund/step_definitions/DeckStepDefs.java
@@ -0,0 +1,48 @@
+package com.symund.step_definitions;
+
+import com.symund.pages.DeckPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class DeckStepDefs {
+
+    DeckPage deckPage = new DeckPage();
+
+    @Given("the user navigates to the Deck page")
+    public void the_user_navigates_to_the_deck_page() {
+        deckPage.navigateToPage("deck");
+    }
+
+    @When("the user creates a new board with name {string}")
+    public void the_user_creates_a_new_board_with_name(String boardName) {
+        deckPage.createNewBoard(boardName);
+    }
+
+    @Then("the new board {string} should be visible in the list of boards")
+    public void the_new_board_should_be_visible_in_the_list_of_boards(String boardName) {
+        Assert.assertTrue(deckPage.isBoardVisibleInListOfBoards(boardName));
+    }
+
+    @When("the user creates a new list under board {string} with name {string}")
+    public void the_user_creates_a_new_list_under_board_with_name(String boardName, String listName) {
+        deckPage.createNewList(boardName,listName);
+    }
+
+    @Then("the new list {string} should be visible under the selected board")
+    public void the_new_list_should_be_visible_under_the_selected_board(String listName) {
+        Assert.assertTrue(deckPage.isListVisibleUnderBoard(listName));
+    }
+
+    @And("the user adds a new card {string} to list {string}")
+    public void the_user_adds_a_new_card_to_list(String cardName, String listName) {
+        deckPage.createNewCard(cardName,listName);
+    }
+
+    @Then("the new card {string} should be visible in list {string}")
+    public void the_new_card_task_should_be_visible_in_list(String cardName, String listName) {
+        Assert.assertTrue(deckPage.isCardVisibleUnderList(cardName,listName));
+    }
+}
