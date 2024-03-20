4bf9fdc9eb90e84e426e42cb68aebef90c1dfb8a
European01 <emiryaziciofficial@gmail.com>
2024-03-20 05:22:07 -0400

    Update DeckPage.java class.

--- src/test/java/com/symund/pages/DeckPage.java
+++ src/test/java/com/symund/pages/DeckPage.java
@@ -1,7 +1,6 @@
 package com.symund.pages;
 
 import com.symund.utilities.BrowserUtils;
-import com.symund.utilities.ConfigurationReader;
 import com.symund.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -29,26 +28,103 @@ public class DeckPage extends BasePage {
     private WebElement appNavigationToggle;
 
     @FindBy(css = "ul.app-navigation-entry__children li")
-    private List<WebElement> boardElements;
+    private List<WebElement> allBoardsInList;
 
-    public void createNewBoard(String boardName) {
+    @FindBy(css = "div#stack-add")
+    private WebElement addListButton;
 
-        appNavigationToggle.click();
-        addBoardModule.click();
-        inputBoardName.sendKeys(boardName);
-        confirmIcon.click();
+    @FindBy(xpath = "(//input[@id='new-stack-input-main'])[1]")
+    private WebElement listNameInputBox;
 
+    @FindBy(xpath = "//div[@id='stack-add']//input[@type='submit']")
+    private WebElement listNameSubmitArrow;
+
+    @FindBy(css = "#new-stack-input-main")
+    private WebElement cardNameInputBox;
+
+    @FindBy(xpath = "//div[@class='stack__card-add']//input[@type='submit']")
+    private WebElement cardNameSubmitArrow;
+
+    public void clickOnBoard(String boardName) {
+
+        WebElement board = Driver.getDriver().findElement(
+                By.xpath("//span[@title='" + boardName + "']/.."));
+
+        BrowserUtils.waitForClickable(board,10);
+        board.click();
     }
 
-    public boolean isBoardVisibleInList(String boardName) {
+    public void clickAddCardButtonByListName(String listName) {
 
-        BrowserUtils.waitForPresenceOfElement(By.cssSelector("span[title='"+boardName+"']"),10);
+        WebElement addButton = Driver.getDriver().findElement(
+                By.xpath("//h3[contains(text(),'"+listName+"')]/../button"));
 
-        for (WebElement boardElement : boardElements) {
-            if (boardElement.getText().equals(boardName)) {
+        BrowserUtils.waitForClickable(addButton,10);
+        addButton.click();
+    }
+
+    public boolean isListVisibleUnderBoard(String listName) {
+
+        WebElement list = Driver.getDriver().findElement(
+                By.xpath("//div[@class='stack__header']//h3[contains(text(),'" + listName + "')]"));
+
+        BrowserUtils.waitForVisibility(list,10);
+        return list.isDisplayed();
+    }
+
+    public boolean isBoardVisibleInListOfBoards(String boardName) {
+
+        WebElement board = Driver.getDriver().findElement(
+                By.xpath("//span[@title='" + boardName + "']"));
+
+        BrowserUtils.waitForVisibility(board,10);
+
+        for (WebElement eachBoard : allBoardsInList) {
+            if (eachBoard.getText().equals(boardName)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public boolean isCardVisibleUnderList(String cardName, String listName) {
+
+        String xpathExpression = "//h3[contains(text(),'" + listName + "')]" +
+                "/../following-sibling::div//span[contains(text(),'" + cardName + "')]";
+
+        WebElement card = Driver.getDriver().findElement(By.xpath(xpathExpression));
+
+        BrowserUtils.waitForVisibility(card, 10);
+        return card.isDisplayed();
+    }
+
+
+
+    public void createNewList(String boardName, String listName) {
+
+        clickAppNavigationToggle();
+        clickOnBoard(boardName);
+        addListButton.click();
+        listNameInputBox.sendKeys(listName);
+        listNameSubmitArrow.click();
+    }
+
+    public void createNewBoard(String boardName) {
+
+        clickAppNavigationToggle();
+        addBoardModule.click();
+        inputBoardName.sendKeys(boardName);
+        confirmIcon.click();
+    }
+
+    public void createNewCard(String cardName, String listName) {
+
+        clickAddCardButtonByListName(listName);
+        cardNameInputBox.sendKeys(cardName);
+        cardNameSubmitArrow.click();
+    }
+
+    public void clickAppNavigationToggle() {
+        appNavigationToggle.click();
     }
 }
