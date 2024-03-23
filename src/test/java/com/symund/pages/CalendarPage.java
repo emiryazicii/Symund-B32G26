d7d33633909b278675cf7951ccb95d653929cba4
zhyldyz <jikarai17@gmail.com>
2024-03-23 18:23:58 -0400

    add Calendar module functionality

--- src/test/java/com/symund/pages/CalendarPage.java
+++ src/test/java/com/symund/pages/CalendarPage.java
@@ -1,4 +1,92 @@
 package com.symund.pages;
 
-public class CalendarPage {
+import com.symund.utilities.BrowserUtils;
+import com.symund.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CalendarPage extends BasePage {
+
+    public CalendarPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    Actions actions = new Actions(Driver.getDriver());
+
+    @FindBy(xpath = "(//button[@aria-label='Actions'])[1]")
+    public WebElement dropdownBtn;
+
+    @FindBy(xpath = "(//button[@class='action-button focusable'])[6]")
+    public WebElement dailyCalendar;
+
+    @FindBy(xpath = "(//button[@class='action-button focusable'])[7]")
+    public WebElement weeklyCalendar;
+
+    @FindBy(xpath = "(//button[@class='action-button focusable'])[8]")
+    public WebElement monthlyCalendar;
+
+    @FindBy(xpath = "(//button[contains(@class, 'primary')])[1]")
+    public WebElement newEventBtn;
+
+    @FindBy(xpath = "//input[@placeholder='Event title']")
+    public WebElement eventTitle;
+
+    @FindBy(xpath = "(//input[@name='date'])[2]")
+    public WebElement fromDateTime;
+
+    @FindBy(xpath = "(//input[@name='date'])[3]")
+    public WebElement toDateTime;
+
+    @FindBy(xpath = "(//button[contains(@class, 'primary')])[2]")
+    public WebElement saveBtn;
+
+    @FindBy(xpath = "//span[@title='Personal']")
+    public WebElement personalBtn;
+
+    public void enterEventName(String eventName) {
+
+        BrowserUtils.waitFor(5);
+        actions.click(newEventBtn).perform();
+        BrowserUtils.waitFor(5);
+        eventTitle.sendKeys(eventName);
+    }
+
+    public void chooseFromDate(String date, String time, String amPm) {
+        BrowserUtils.waitFor(5);
+
+        // Highlight the fromDate string
+        BrowserUtils.executeJsCommand(fromDateTime, "arguments[0].select();");
+     //   BrowserUtils.waitFor(1);
+
+        // Delete the fromDate string
+        fromDateTime.sendKeys(Keys.DELETE);
+     //   BrowserUtils.waitFor(5);
+        fromDateTime.sendKeys("from " + date + " at " + time + " " + amPm);
+    }
+
+    public void chooseToDate(String date, String time, String amPm) {
+
+        actions.click(toDateTime).perform();
+
+        // Highlight the toDate string
+        BrowserUtils.executeJsCommand(toDateTime, "arguments[0].select();");
+
+        actions.sendKeys(Keys.DELETE).perform();
+
+        toDateTime.sendKeys("to " + date + " at " + time + " " + amPm);
+
+        actions.click(saveBtn).perform();
+    }
+    public boolean isEventVisible(String date, String time) {
+
+        WebElement event = Driver.getDriver().findElement(
+                By.xpath("//td[@data-date='" + date + "']//div[.='" + time + "']/.."));
+
+        BrowserUtils.waitFor(5);
+
+        return event.isDisplayed();
+    }
 }
---
+++ src/test/java/com/symund/step_definitions/CalendarModuleFunctionality_StepDef.java
@@ -0,0 +1,95 @@
+package com.symund.step_definitions;
+
+import com.symund.pages.CalendarPage;
+import com.symund.pages.LoginPage;
+import com.symund.utilities.BrowserUtils;
+import com.symund.utilities.ConfigurationReader;
+import com.symund.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class CalendarModuleFunctionality_StepDef {
+    CalendarPage calendarPage = new CalendarPage();
+
+    @When("user navigates to the Calendar module page")
+    public void user_navigates_to_the_calendar_module_page() {
+        calendarPage.navigateToPage("calendar");
+    }
+    @Given("user navigates to the dropdown option")
+    public void user_navigates_to_the_dropdown_option() {
+
+        calendarPage.dropdownBtn.click();
+    }
+    @When("user clicks to the Daily calendar")
+    public void user_clicks_to_the_daily_calendar() {
+
+        calendarPage.dailyCalendar.click();
+    }
+    @Then("Daily Calendar should be visible on the page")
+    public void daily_calendar_should_be_visible_on_the_page() {
+
+        String dailyCalendarURL = Driver.getDriver().getCurrentUrl();
+        String expectedInDailyURL = "timeGridDay";
+        Assert.assertTrue(dailyCalendarURL.contains(expectedInDailyURL));
+    }
+    @When("user clicks to the Weekly calendar")
+    public void user_clicks_to_the_weekly_calendar() {
+
+        calendarPage.weeklyCalendar.click();
+    }
+    @Then("Weekly Calendar should be visible on the page")
+    public void weekly_calendar_should_be_visible_on_the_page() {
+
+        String weeklyCalendarURL = Driver.getDriver().getCurrentUrl();
+        String expectedInWeeklyURL = "timeGridWeek";
+        Assert.assertTrue(weeklyCalendarURL.contains(expectedInWeeklyURL));
+    }
+    @When("user clicks to the Monthly calendar")
+    public void user_clicks_to_the_monthly_calendar() {
+
+        calendarPage.monthlyCalendar.click();
+    }
+    @Then("Monthly Calendar should be visible on the page")
+    public void monthly_calendar_should_be_visible_on_the_page() {
+
+        String monthlyCalendarURL = Driver.getDriver().getCurrentUrl();
+        String expectedInMonthlyURL = "dayGridMonth";
+        Assert.assertTrue(monthlyCalendarURL.contains(expectedInMonthlyURL));
+    }
+
+    @When("user enters event name {string},")
+    public void userEntersEventName(String eventName) {
+        calendarPage.enterEventName(eventName);
+    }
+
+    @And("user enters starting date {string}, {string},{string}")
+    public void userEntersStartingDate(String date, String time, String amPm) {
+        calendarPage.chooseFromDate(date, time, amPm);
+    }
+
+    @And("user enters ending date {string}, {string},{string} and save it")
+    public void userEntersEndingDateAndSaveIt(String date, String time, String amPm) {
+        calendarPage.chooseToDate(date, time, amPm);
+    }
+
+    @Then("user should see the new event with given {string} and {string} through the Monthly Calendar view")
+    public void userShouldSeeTheNewEventWithGivenAndThroughTheMonthlyCalendarView(String date, String time) {
+
+        Assert.assertTrue(calendarPage.isEventVisible(date, time));
+    }
+
+
+
+
+}
+
+
+
+
+
